f44c6a79333e3041f7f2c128f8d5ec91e475391d
jasony62 <jasony62@qq.com>
2020-04-27 17:49:34 +0800

    支持https

--- ffmpeg/config/app.js
+++ ffmpeg/config/app.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: 3000,
+  https: { port: 3443, key: process.env.ssl_certificate_key, cert: process.env.ssl_certificate },
   name: 'tms-koa-ffmpeg-demo',
   router: {
     controllers: {
--- ffmpeg/config/push.js
+++ ffmpeg/config/push.js
@@ -1,3 +1,3 @@
 module.exports = {
-  port: 3001,
+  https: { port: 3444, key: process.env.ssl_certificate_key, cert: process.env.ssl_certificate },
 }
